82f6fcce358937209f1216627c0359faf57ab2f6
Charles Tsai <charles_tsai@symphox.net>
2023-08-01 19:05:06 +0800

    reslove two sum

---
+++ 1.two-sum.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const map: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (map.has(diff)) {
+      return [map.get(diff)!, i];
+    }
+    map.set(nums[i], i);
+  }
+
+  return [];
+}
+// @lc code=end
