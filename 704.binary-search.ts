3b19cc7d6c5e031b4334f8d0cf12cb51647f4bbe
Charles Tsai <charles_tsai@symphox.net>
2023-08-21 15:08:51 +0800

    resolve 704

---
+++ 704.binary-search.ts
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  const index = nums.indexOf(target);
+  console.log(index);
+  return index;
+}
+// @lc code=end
