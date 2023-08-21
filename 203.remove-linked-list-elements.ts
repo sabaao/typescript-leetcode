db9e4c3ff3c1de4b22e5a7e6d34fdd22bafbb7f7
Charles Tsai <charles_tsai@symphox.net>
2023-08-21 17:37:50 +0800

    resolve 203

---
+++ 203.remove-linked-list-elements.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=203 lang=typescript
+ *
+ * [203] Remove Linked List Elements
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  if (head === null) {
+    return null;
+  }
+  let current = head;
+  while (current.next) {
+    if (current.next.val === val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return head.val === val ? head.next : head;
+}
+// @lc code=end
