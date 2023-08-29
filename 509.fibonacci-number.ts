4cf24bdcd240e66b468a18f10d9af3dbf750266b
Charles Tsai <charles_tsai@symphox.net>
2023-08-29 11:19:45 +0800

    resolve 509

---
+++ 509.fibonacci-number.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=509 lang=typescript
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+function fib(n: number): number {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  const dp: number[] = [];
+  dp[0] = 0;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+// @lc code=end
